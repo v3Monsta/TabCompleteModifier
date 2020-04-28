621fd368cc991ec4f7ae1ae3dd7da994bdac50e3
v3Monsta <16313385+v3Monsta@users.noreply.github.com>
2020-04-28 19:03:34 -0400

    Fixed commands not being removed, added ability to whitelist commands

--- src/main/java/me/v3monsta/tabcompletemodifier/listener/TabCompleteListener.java
+++ src/main/java/me/v3monsta/tabcompletemodifier/listener/TabCompleteListener.java
@@ -4,17 +4,25 @@ import me.v3monsta.tabcompletemodifier.TabCompleteModifier;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.server.TabCompleteEvent;
+import org.bukkit.event.player.PlayerCommandSendEvent;
 
+import java.util.Collection;
 import java.util.List;
-
 
 public class TabCompleteListener implements Listener {
     private final TabCompleteModifier INSTANCE = TabCompleteModifier.getInstance();
+    private final List<String> COMMANDS = INSTANCE.getConfig().getStringList("commands");
+    private final boolean USE_AS_WHITELIST = INSTANCE.getConfig().getBoolean("use-as-whitelist");
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onTabComplete(TabCompleteEvent event) {
-        List<String> commands = INSTANCE.getConfig().getStringList("commands");
-        event.getCompletions().removeIf(commands::contains);
+    public void onTabComplete(PlayerCommandSendEvent event) {
+        Collection<String> commands = event.getCommands();
+        if (USE_AS_WHITELIST) {
+            commands.clear();
+            commands.addAll(COMMANDS);
+            return;
+        }
+
+        commands.removeAll(COMMANDS);
     }
 }
